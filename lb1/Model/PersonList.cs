60aea5c9283ce2a281b4d0bdc57408dd31dc1c1b
Илья Соболев <Sobolev0208@outlook.com>
2023-03-23 17:47:35 +0700

    Добавлен метод проверки допустимости значения индекса и массив персон

--- lb1/Model/PersonList.cs
+++ lb1/Model/PersonList.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Model
 {
+    /// <summary>
+    /// Класс PersonList.
+    /// </summary>
     internal class PersonList
     {
+        /// <summary>
+        /// Массив персон.
+        /// </summary>
+        private Peson[] _personsArray = new Person[0];
+        public void AddPerson(Person person) 
+        {
+            var newPersonsIndex = newPersonsIndex.Length;
+            Array.Resize(ref _personsArray, newPersonsIndex + 1);
+            _personsArray[newPersonsIndex] = person;
+        }
+
+        /// <summary>
+        /// Метод проверки допустимости значения индекса.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        private void IndexInArray(int index)
+        {
+            if (index < 0 || index >= _personsArray.Length)
+            { 
+                throw new IndexOutOfRangeException("Ошибка: индекс вне допустимого диапазона!");
+            }
+        }
     }
 }
