1f4c1f54027570ae398200bae337d9dd55491e88
Илья Соболев <Sobolev0208@outlook.com>
2023-03-23 11:02:20 +0700

    Создан конструктор, метод вывода информации о пользователе. Осуществлена проверка на контроль исключения (пустое имя).

---
+++ lb1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Подключение namespace *Model* к проекту
+using Model;
+
+using System.Diagnostics;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // string.Empty - передача пустой строки
+                Person person = new Person(string.Empty, "Test1", 23);
+            }
+            catch
+            {
+                Console.WriteLine("Name is empty!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lb1/Model/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Model
+{
+    public class Person
+    {
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Фамилия.
+        /// </summary>
+        private string _surname;
+
+        /// <summary>
+        /// Возраст.
+        /// </summary>
+        private int _age;
+
+        /// <summary>
+        /// Вывод информации о пользователе.
+        /// </summary>
+        /// <returns></returns>
+        public string GetInfo()
+        {
+            // Вывод информации о пользователе
+            return $"Person info:\n" +
+            $"name: {this._name}\n" +
+            $"surname: {this._surname}\n" +
+            $"age: {this._age}";
+        }
+
+        /// <summary>
+        /// Конструктор для инициализации данных о пользователе.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        /// <param name="age"></param>
+        public Person(string name, string surname, int age)
+        {
+            _name = name;
+            _surname = surname;
+            _age = age;
+
+            Regex regex = new Regex("");
+            // Если не соответствует шаблону, кинуть исключение!
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new System.ArgumentException("Name is`t correct!");
+            }
+
+        }
+    }
+
+
+}
