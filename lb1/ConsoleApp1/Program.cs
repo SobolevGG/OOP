5dd50308cfc18ddbd301cf47e9196f969239a730
sobolev638 <sobolev638@gmail.com>
2023-12-26 01:29:04 +0700

    Настройка консольного вывода .

--- lb1/ConsoleApp1/Program.cs
+++ lb1/ConsoleApp1/Program.cs
@@ -15,13 +15,89 @@ namespace ConsoleApp1
         /// </summary>
         public static void Main()
         {
-            var runningCalculator = new RunCalc(65, 0.1, 5, Intensity.Sprinting);
+            var egWeight = 65;
+            var egMetCoef = 0.1;
+            var egDuration = 10;
+            var egDistance = 10;
+            var egIntensity = Intensity.Sprinting;
+            var egStyle = Style.Butterfly;
+
+            Console.WriteLine($"    Пример расчёта калорий " +
+                $"для человека:\n      " +
+                $"с массой {egWeight} кг;\n      " +
+                $"с интенсивностью тренировки: {egIntensity}");
+
+            var runningCalculator = new RunCalc(egWeight,
+                                                egMetCoef,
+                                                egDistance,
+                                                egIntensity);
             double runningCalories = runningCalculator.CalculateCalories();
+
             Console.WriteLine($"\n    Затрачиваемые калории при беге: {runningCalories}");
 
-            var swimmingCalculator = new SwimCalc(65, 0.1, 5, 4, Style.Butterfly);
+            var swimmingCalculator = new SwimCalc(egWeight,
+                                                  egMetCoef,
+                                                  egDuration,
+                                                  egDistance,
+                                                  egStyle);
+
             double swimmingCalories = swimmingCalculator.CalculateCalories();
-            Console.WriteLine($"    Калории при плавании: {swimmingCalories}");
+            Console.WriteLine($"    Затрачиваемые калории при плавании: {swimmingCalories}");
+
+            Console.WriteLine("\n    Для начала расчёта" +
+                " сжигаемых калорий выберите тренировку:" +
+                "\n      1 - Плавание;" +
+                "\n      2 - Бег;" +
+                "\n      3 - Жим штанги;" +
+                "\n      4 - Отмена.");
+
+            while (true)
+            {
+                Console.Write("\nВведите цифру из представленного " +
+                    "списка: ");
+                var consoleKey = Console.ReadLine();
+                switch (consoleKey)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("\tОплата по окладу");
+
+                            // PrintWages(WagesBySalary());
+                            break;
+                        }
+
+                    case "2":
+                        {
+                            Console.WriteLine("\tОплата по часовой " +
+                                "тарифной ставке");
+
+                            // PrintWages(WagesAtTheHourlyTariffRate());
+                            break;
+                        }
+
+                    case "3":
+                        {
+                            Console.WriteLine("\tОплата по тарифной " +
+                                "ставке");
+
+                            // PrintWages(WagesAtTheTariffRate());
+                            break;
+                        }
+
+                    case "4":
+                        {
+                            Environment.Exit(4);
+                            break;
+                        }
+
+                    default:
+                        {
+                            Console.WriteLine("Попробуйте еще раз");
+                        }
+
+                        break;
+                }
+            }
         }
     }
 }
