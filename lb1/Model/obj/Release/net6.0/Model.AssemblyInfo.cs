e58a3441e0cffb8a55709cea49a739701529c621
sobolev638 <sobolev638@gmail.com>
2024-01-06 00:35:47 +0700

    Точка отката (до TODO)

--- lb1/Model/obj/Release/net6.0/Model.AssemblyInfo.cs
+++ lb1/Model/obj/Release/net6.0/Model.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e4c34b982760fd3664ec31d917ac3b38db8a1db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
