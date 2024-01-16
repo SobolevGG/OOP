38023bcdf95d2890e9aa0a89ea6688025e075e81
sobolev638 <sobolev638@gmail.com>
2024-01-16 17:02:30 +0700

    Методы работы со списком характеристик выделены в отдельный класс ListFormulas

---
+++ ConsoleApp1/ConsoleApp1/ListFormulas.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HydroGeneratorOptimization.Formulas;
+
+namespace HydroGeneratorOptimization
+{
+    public class ListFormulas
+    {
+        /// <summary>
+        /// Создание нового документа и ввод новой формулы.
+        /// </summary>
+        public static void CreateNewListAndAddForm(string formulaExpression, string nameFormula)
+        {
+            string newFormulaExpression = formulaExpression;
+
+            var powerFormulaFileManager = new XmlFileManager<List<PowerFormula>>();
+
+            // Создание нового списка формул
+            List<PowerFormula> powerFormulas = new List<PowerFormula>();
+
+            // Добавление новой формулы в список
+            PowerFormula newFormula = new PowerFormula
+            {
+                Name = nameFormula, // Вы можете выбрать любое уникальное имя
+                FormulaExpression = newFormulaExpression
+            };
+
+            powerFormulas.Add(newFormula);
+
+            // Сохранение списка формул в XML-файл
+            powerFormulaFileManager.Save("newFormulas.xml", powerFormulas);
+        }
+
+        /// <summary>
+        /// Ввод новой формулы в список.
+        /// </summary>
+        public static void AddInListNewForm(string newFormulaExpression, string nameNewFormula)
+        {
+            // Создаем экземпляр XmlFileManager для List<PowerFormula>
+            var powerFormulaFileManager = new XmlFileManager<List<PowerFormula>>();
+
+            // Загрузка файла формул
+            List<PowerFormula> powerFormulas = powerFormulaFileManager.Load("formulas.xml");
+
+            // Добавление новой формулы в список
+            PowerFormula newFormula = new PowerFormula
+            {
+                Name = nameNewFormula, // Вы можете выбрать любое уникальное имя
+                FormulaExpression = newFormulaExpression
+            };
+
+            powerFormulas.Add(newFormula);
+
+            // Сохранение обновленного списка формул в XML-файл
+            powerFormulaFileManager.Save("formulas.xml", powerFormulas);
+        }
+
+        /// <summary>
+        /// Метод удаления формулы из списка и последующего сохранения в XML-документ.
+        /// </summary>
+        /// <param name="powerFormulas">Список формул.</param>
+        /// <param name="index">Номер формулы.</param>
+        public static void RemoveFormByNum(List<PowerFormula> powerFormulas, int index)
+        {
+            if (powerFormulas != null && index >= 0 && index < powerFormulas.Count)
+            {
+                powerFormulas.RemoveAt(index);
+
+                // Сохранение обновленного списка формул в XML-файл
+                var powerFormulaFileManager = new XmlFileManager<List<PowerFormula>>();
+                powerFormulaFileManager.Save("formulas.xml", powerFormulas);
+            }
+            else
+            {
+                Console.WriteLine("Invalid index or formulas list is null.");
+            }
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -46,79 +46,8 @@ public class Program
 
         // AddInListNewForm("Qi * (96.7 - (Math.Pow(Math.Abs(Qi - 494), 1.78) / Math.Pow(22.5, 2) + Math.Pow(Math.Abs(head - 91), 1.5) / Math.Pow(4, 2)))", "NewFormula321");
 
-        // Пример удаления формулы с индексом 2
-        RemoveFormByNum(powerFormulaFileManager.Load("formulas.xml"), 14);
+        // Пример удаления формулы с индексом 14
+        ListFormulas.RemoveFormByNum(powerFormulaFileManager.Load("formulas.xml"), 14);
 
     }
-
-    /// <summary>
-    /// Создание нового документа и ввод новой формулы.
-    /// </summary>
-    public static void CreateNewListAndAddForm(string formulaExpression, string nameFormula)
-    {
-        string newFormulaExpression = formulaExpression;
-
-        var powerFormulaFileManager = new XmlFileManager<List<PowerFormula>>();
-
-        // Создание нового списка формул
-        List<PowerFormula> powerFormulas = new List<PowerFormula>();
-
-        // Добавление новой формулы в список
-        PowerFormula newFormula = new PowerFormula
-        {
-            Name = nameFormula, // Вы можете выбрать любое уникальное имя
-            FormulaExpression = newFormulaExpression
-        };
-
-        powerFormulas.Add(newFormula);
-
-        // Сохранение списка формул в XML-файл
-        powerFormulaFileManager.Save("newFormulas.xml", powerFormulas);
-    }
-
-    /// <summary>
-    /// Ввод новой формулы в список.
-    /// </summary>
-    public static void AddInListNewForm(string newFormulaExpression, string nameNewFormula)
-    {
-        // Создаем экземпляр XmlFileManager для List<PowerFormula>
-        var powerFormulaFileManager = new XmlFileManager<List<PowerFormula>>();
-
-        // Загрузка файла формул
-        List<PowerFormula> powerFormulas = powerFormulaFileManager.Load("formulas.xml");
-
-        // Добавление новой формулы в список
-        PowerFormula newFormula = new PowerFormula
-        {
-            Name = nameNewFormula, // Вы можете выбрать любое уникальное имя
-            FormulaExpression = newFormulaExpression
-        };
-
-        powerFormulas.Add(newFormula);
-
-        // Сохранение обновленного списка формул в XML-файл
-        powerFormulaFileManager.Save("formulas.xml", powerFormulas);
-    }
-
-    /// <summary>
-    /// Метод удаления формулы из списка и последующего сохранения в XML-документ.
-    /// </summary>
-    /// <param name="powerFormulas">Список формул.</param>
-    /// <param name="index">Номер формулы.</param>
-    public static void RemoveFormByNum(List<PowerFormula> powerFormulas, int index)
-    {
-        if (powerFormulas != null && index >= 0 && index < powerFormulas.Count)
-        {
-            powerFormulas.RemoveAt(index);
-
-            // Сохранение обновленного списка формул в XML-файл
-            var powerFormulaFileManager = new XmlFileManager<List<PowerFormula>>();
-            powerFormulaFileManager.Save("formulas.xml", powerFormulas);
-        }
-        else
-        {
-            Console.WriteLine("Invalid index or formulas list is null.");
-        }
-    }
-
 }
