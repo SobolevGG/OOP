6749fad4d7a2f2a5092a8a98330795f2d8cc9091
sobolev638 <sobolev638@gmail.com>
2024-02-03 16:51:57 +0700

    добавлены проверки через свойства

--- ConsoleApp1/ConsoleApp1/HydroUnit.cs
+++ ConsoleApp1/ConsoleApp1/HydroUnit.cs
@@ -9,16 +9,67 @@ namespace Model
 {
     public class HydroUnit
     {
+        private string name;
+        private double waterFlow;
+        private double powerLoad;
+        private string status;
+
         [DisplayName("ГА")]
-        public string Name { get; set; }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
 
         [DisplayName("Расход воды, м. куб/с")]
-        public double WaterFlow { get; set; }
+        public double WaterFlow
+        {
+            get { return waterFlow; }
+            set
+            {
+                if (value >= 0)
+                {
+                    waterFlow = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Расход воды не может быть отрицательным.");
+                }
+            }
+        }
 
         [DisplayName("Загрузка, МВт")]
-        public double PowerLoad { get; set; }
+        public double PowerLoad
+        {
+            get { return powerLoad; }
+            set
+            {
+                if (value >= 0 && value <= 508)
+                {
+                    powerLoad = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Загрузка должна быть в диапазоне от 0 до 508 МВт.");
+                }
+            }
+        }
 
         [DisplayName("Статус")]
-        public string Status { get; set; }
+        public string Status
+        {
+            get { return status; }
+            set
+            {
+                if (value == "В работе" || value == "Выведен")
+                {
+                    status = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Статус должен быть 'В работе' или 'Выведен'.");
+                }
+            }
+        }
     }
 }
