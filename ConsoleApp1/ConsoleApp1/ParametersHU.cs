1d46c93c7fc44f7fa007a74b908e01aecf4bfff7
sobolev638 <sobolev638@gmail.com>
2024-02-03 17:29:28 +0700

    добавлены исключения в проверках в свойствах

--- ConsoleApp1/ConsoleApp1/ParametersHU.cs
+++ ConsoleApp1/ConsoleApp1/ParametersHU.cs
@@ -18,25 +18,53 @@ namespace Model
         public string HU
         {
             get { return _HU; }
-            set { _HU = !string.IsNullOrEmpty(value) ? value : "defaultHU"; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Значение не может быть пустым.", nameof(HU));
+                }
+                _HU = value;
+            }
         }
 
         public double Load
         {
             get { return _load; }
-            set { _load = (value >= 0 && value <= 508) ? value : 0; }
+            set
+            {
+                if (value < 0 || value > 508)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(Load), "Значение должно быть в диапазоне от 0 до 508.");
+                }
+                _load = value;
+            }
         }
 
         public int Zone
         {
             get { return _zone; }
-            set { _zone = (value == 1 || value == 3) ? value : 1; }
+            set
+            {
+                if (value != 1 && value != 3)
+                {
+                    throw new ArgumentException("Значение должно быть 1 или 3.", nameof(Zone));
+                }
+                _zone = value;
+            }
         }
 
         public string Status
         {
             get { return _status; }
-            set { _status = (value == "Выведен") ? "Выведен" : "В работе"; }
+            set
+            {
+                if (value != "Выведен" && value != "В работе")
+                {
+                    throw new ArgumentException("Недопустимое значение для столбца 'Статус'.", nameof(Status));
+                }
+                _status = value;
+            }
         }
     }
 }
